a8788d9e948257712fb6807ab1e91735ee5f0c6f
Moein Siami <moeins1216@gmail.com>
2024-11-18 21:46:24 +0330

    some changes

--- store/views.py
+++ store/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from store.models import Product
+
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    newest_products = Product.objects.order_by('-created')[:10]
+    context = {
+        'products': products,
+        'newest_products': newest_products
+    }
+    return render(request, 'index.html', context)
+
+
+def contact_us(request):
+    return render(request, 'contact-us.html')
