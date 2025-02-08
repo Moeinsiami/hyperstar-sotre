28b86ebd0a84f498142372b925b0bb2aa2b1645c
Moein Siami <moeins1216@gmail.com>
2025-02-08 04:41:59 -0600

    add static

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'store'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('contact-us', views.contact_us, name='contact_us'),
+    # path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    # path('contact-us', views.contact_us, name='contact_us'),
 ]
